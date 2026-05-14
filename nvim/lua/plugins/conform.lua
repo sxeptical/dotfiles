d09534deb18185a67966c4edf186a21e2c6ad870
Steven <steventokms@gmail.com>
2026-05-14 22:51:51 +0800

    add nvim config

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      json = { "prettier" },
+      python = { "black" },
+    },
+
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
