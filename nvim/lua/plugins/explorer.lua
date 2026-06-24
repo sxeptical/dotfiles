f1557c387b47e147860a29c5cc6abc1bdc1b04fe
Steven <steventokms@gmail.com>
2026-06-25 02:10:36 +0800

    nvim: add explorer plugin config, enable luasnip, update lockfile
    - Add lua/plugins/explorer.lua: configure snacks.nvim explorer to show hidden files
    - Enable lazyvim.plugins.extras.coding.luasnip in lazyvim.json
    - Drop format_on_save block from conform.lua (handled by LazyVim)
    - Refresh lazy-lock.json to latest plugin commits

---
+++ nvim/lua/plugins/explorer.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      explorer = {
+        -- show hidden files (dotfiles like .git, .env, .DS_Store) by default
+        hidden = true,
+      },
+    },
+  },
+}
